c3d3bbc4f650d42a3f3ab2a93f8bdab74facec3d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-03-27 15:19:32 +0100

    fix: limitInputPixels

--- api/transformer.js
+++ api/transformer.js
@@ -6,15 +6,23 @@ import { URL } from "node:url";
 const tryJSON = (value) => {
   try {
     return JSON.parse(value);
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
     return null;
   }
-}
+};
+
+let resolve;
+let promise;
+const isLocal = false;
 
 export default async (req, res) => {
   const { url: src, options, ...queryOptions } = req.query;
+
+  if (isLocal) {
+    await promise;
+    promise = new Promise((res) => resolve = res);
+  }
 
   if (!src) return res.status(400).send("Image URL is required");
 
@@ -23,20 +31,23 @@ export default async (req, res) => {
     const filename = url.pathname.split("/").pop();
     const stream = (await axios({ url, responseType: "stream" })).data;
     const sharpInstance = stream.pipe(
-      (options?.length > 1 && tryJSON(options)) ? sharp(JSON.parse(options)) : sharp()
+      (options?.length > 1 && tryJSON(options))
+        ? sharp({ limitInputPixels: false, ...JSON.parse(options) })
+        : sharp({ limitInputPixels: false }),
     );
 
-    for (const [option, value] of Object.entries(queryOptions))
+    for (const [option, value] of Object.entries(queryOptions)) {
       if (typeof sharpInstance[option] === "function") {
         const object = value ? JSON.parse(value) : null;
         const options = Array.isArray(object) ? object : object ? [object] : [];
         sharpInstance[option](...options);
       }
+    }
 
     const formatOut = sharpInstance.options.formatOut;
     const newFilename = filename.replace(
       /(\.)([^.]*?)$/,
-      (...a) => `${a[1]}${formatOut === "input" ? a[2] : formatOut}`
+      (...a) => `${a[1]}${formatOut === "input" ? a[2] : formatOut}`,
     );
 
     sharpInstance.pipe(
@@ -44,10 +55,14 @@ export default async (req, res) => {
         "Content-Type": lookup(newFilename) ?? "image/*",
         "Cache-Control": `s-maxage=${86400 * 30}`,
         "Content-Disposition": `inline; filename="${newFilename}"`,
-      })
+      }),
     );
   } catch (error) {
     console.error("error", error);
     res.status(500).send("Error processing image");
+  }
+
+  if (resolve) {
+    resolve();
   }
 };
